1707097679e6fd30f0c7e9440a6065205de89630
azeez92 <azeezsodiq18@gmail.com>
2024-12-10 19:49:28 +0100

    Component creation

--- Atibyan-blog/src/App.jsx
+++ Atibyan-blog/src/App.jsx
@@ -5,11 +5,14 @@ import Dashboard from "./pages/Dashboard"
 import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import Projects from "./pages/Projects"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
     
 <BrowserRouter>
+<Header />
 <Routes>
 <Route  path="/"  element={<Home />}/>
 <Route  path="/about"  element={<About />}/>
@@ -17,11 +20,8 @@ export default function App() {
 <Route  path="/sign-in"  element={<Signin />}/>
 <Route  path="sign-up"  element={<Signup />}/>
 <Route  path="/projects"  element={<Projects />}/>
-
-
-
 </Routes>
-
+<Footer />
 </BrowserRouter>
 
 
