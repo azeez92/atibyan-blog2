f1b68cf204d107556cfdca3b83828d2c7c622d9a
azeez92 <azeezsodiq18@gmail.com>
2024-12-29 23:27:48 +0100

    middleware function

--- backend/index.js
+++ backend/index.js
@@ -31,3 +31,15 @@ app.use('/api/user', UserRoutes);
 app.use('/api/auth', authRoutes);
 
 
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server Error'
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+
+});
+
